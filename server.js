4ed21ac6b3395782423907438fbe7ef717666ed1
Laari_Stephen <lastepcode@gmail.com>
2022-10-14 01:35:38 -0700

    fixed cors policy issue

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const cors = require("cors");
 const workoutRoute = require("./api/routes/workoutRoute");
 const app = express();
 
 //middlewares
+app.use(cors());
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5000");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
 app.use(express.json());
 app.use("/api/workouts", workoutRoute);
 
